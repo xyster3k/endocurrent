c445a3168e986ccb7d5dad7c86cc281ceb97b2ce
Xyster3k <141751440+xyster3k@users.noreply.github.com>
2025-12-17 02:30:13 +0100

    v3

--- web/.cloudflare/dist/cloudflare/init.js
+++ web/.cloudflare/dist/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1765904760625,
+    __BUILD_TIMESTAMP_MS__: 1765934812467,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
